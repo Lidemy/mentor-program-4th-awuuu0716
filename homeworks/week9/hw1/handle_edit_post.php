0aa62f366e520b21d1a933aeae9ce4af22043ad2
awuuu0716 <llinn927716@gmail.com>
2020-08-12 21:43:42 +0800

    add edit_post.php & handle_edit_post.php

---
+++ homeworks/week9/hw1/handle_edit_post.php
@@ -0,0 +1,19 @@
+<?php
+  require_once("utlis.php");
+  $id = $_POST['id'];
+  $comment = htmlspecialchars($_POST["comment"]);
+
+  $sql = sprintf("UPDATE `Awu_comments` SET `comment` = '%s' WHERE `id` = '%s'",
+    $comment,
+    $id);
+
+  $result = $conn->query($sql);
+
+  if (!$result) {
+    die($conn->error);
+  }
+
+  header("Location: index.php")
+
+
+?>
